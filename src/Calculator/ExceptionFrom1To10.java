33f863252a1eb363732ebe944f25d16b53c39a79
Ivan Saushin <ivan.saushin@gmail.com>
2020-11-11 13:33:07 +0300

    added own Exception class

---
+++ src/Calculator/ExceptionFrom1To10.java
@@ -0,0 +1,8 @@
+package Calculator;
+
+public class ExceptionFrom1To10 extends Exception {
+
+    public ExceptionFrom1To10() {
+        System.out.println("Значение должно быть от 1 до 10 !");
+    }
+}
