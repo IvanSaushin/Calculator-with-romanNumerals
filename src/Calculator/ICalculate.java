36d1718520233060e2ceebd96d883fe5fa0e19f2
Ivan Saushin <ivan.saushin@gmail.com>
2020-11-11 12:55:11 +0300

    revisions 2

--- src/Calculator/ICalculate.java
+++ src/Calculator/ICalculate.java
@@ -92,7 +92,8 @@ public interface ICalculate {
 
     enum RomanNumerals{
         I(1), II(2), III(3), IV(4),V(5),
-        VI(6), VII(7), VIII(8), IX(9), X(10);
+        VI(6), VII(7), VIII(8), IX(9), X(10),
+        XL(40), L(50), XC(90), C(100);
         int value;
 
         RomanNumerals(int value) {
